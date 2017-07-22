8949395170352d69162406278c4589a0145bd98c
Nicolas Richard <nrichard@radiumone.com>
2017-07-22 14:02:10 -0700

    1st commit

--- README.md
+++ README.md
@@ -1,1 +1,138 @@
-# map_reduce_stackoverflow_data_analysis
+Stack overflow provides its data here : https://archive.org/download/stackexchange
+
+The "comments" data unzipped is about 15GB.
+
+## What is the most referenced Wikipedia page in Stackoverflow comments ?
+
+# 1. Naively parse the URLs
+
+For each comment, I check if there is a Wikipedia link inside. I parse it out and sent it to the reducer.
+
+The difficult part is the links are not nicely tagged but included in plain format in the body of the comment. 
+
+The problem with this approach is I'm getting a lot of garbage URLs because the links are not nicely tagged but included in plain format in the body of the comment which make parsing them not obvious.
+
+This is a major problem as urls have a tendency to be found at the end of a sentence or between parentheses, or followed by a comma, a dot, any punctuation mark or a permutation of all / any of that.
+
+The same page can have several representations...
+
+https://en.wikipedia.org/wiki/SQL_injection
+https://en.wikipedia.org/wiki/SQL_injection)
+https://en.wikipedia.org/wiki/SQL_injection).
+
+Note: In some cases a parenthesi at the end of a URL is legit and should not be removed...
+
+#2. Check that the URLs are reachable
+
+Before emitting a URL to the reducer I need to make sure it's a good one.
+
+Let's make a GET request to every candidate page, if I receive a 200 status code as a response, I'll consider this URL good.
+
+This is slow... And of course turns out to be very inefficient as it means I connect to the same popular Wikipedia pages over and over again just to figure out that "yes it's a good one".
+
+I was able to run the job this way for a subset of the dataset (first 10k records). The result is a list of validated URLs that we will be re-using later.
+
+#3. rule of thumb to recover dirty URLs
+
+ As I run the job this way I realized I was getting a lot of 404 responses due to the poorly shaped URLs with punctuation marks at the end. More than half of the links were discarded for that reason.
+
+I decided that for every candidate URL I will be testing :
+
+- The URL as parsed
+- The URL minus the last character
+- The URL minus the 2 last characters
+
+Now I was catching most URLs but processing got very very slow.
+
+#4. Distributed Cache
+
+To avoid this the Distributed Cache feature of Map Reduce is on point.
+
+Each mapper upon start will load in-memory a set of knowns good URLs that we obtained the hard way, by connecting to each one and getting the desired 200 response.
+
+With this is place, to validate a URL, first check if it's in the cache.
+
+If it's not, try to connect to it.
+
+My first cache file was created from processing the 10k records, then I created one from 1 million records, then one from 20 million records (about 30% of the data), and from that I was able to analyze the entire dataset in a few hours on my laptop.
+
+
+#5. Gather some stats
+
+Using the Map Reduce context counters it's very easy to count what is going on at every step.
+
+Status 200  11109
+Status 400  1030
+Status 404  11341
+All records scans 58159098
+Cache_Hits  64364
+Wikipedia_URL_Found 77284
+unique URLS 16103
+
+From this we can see that the cache has proven very useful
+cache hit ratio : 83%
+
+200 + cache hits : 75473
+
+Urls found that could not be understood / resolved : 1811 / 2%
+
+#6. The results
+
+This is the top 50 of most common Wikipedia pages found in StackOverflow comments.
+
+Urls / Count
+
+https://en.wikipedia.org/wiki/Prepared_statement 1760
+https://en.wikipedia.org/wiki/SQL_injection 1213
+https://en.wikipedia.org/wiki/Same_origin_policy 577
+https://en.wikipedia.org/wiki/Database_normalization 444
+https://en.wikipedia.org/wiki/Undefined_behavior 399
+https://en.wikipedia.org/wiki/Byte_order_mark 304
+https://en.wikipedia.org/wiki/Singleton_pattern 260
+https://en.wikipedia.org/wiki/ISO_8601 259
+https://en.wikipedia.org/wiki/Floating_point 249
+https://en.wikipedia.org/wiki/Same-origin_policy 231
+https://en.wikipedia.org/wiki/JSONP 230
+https://en.wikipedia.org/wiki/Model%E2%80%93view%E2%80%93controller 215
+https://en.wikipedia.org/wiki/Endianness 208
+https://en.wikipedia.org/wiki/Big_O_notation 208
+https://en.wikipedia.org/wiki/Hash_table 204
+https://en.wikipedia.org/wiki/Liskov_substitution_principle 199
+https://en.wikipedia.org/wiki/Post/Redirect/Get 198
+https://en.wikipedia.org/wiki/Regular_expression 189
+https://en.wikipedia.org/wiki/Base64 186
+https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle 183
+https://en.wikipedia.org/wiki/Cross-site_scripting 178
+https://en.wikipedia.org/wiki/UTF-8 174
+https://en.wikipedia.org/wiki/Levenshtein_distance 173
+https://en.wikipedia.org/wiki/Knapsack_problem 173
+https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern 173
+https://en.wikipedia.org/wiki/Cross-origin_resource_sharing 173
+https://en.wikipedia.org/wiki/Trie 172
+https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes 171
+https://en.wikipedia.org/wiki/Single_responsibility_principle 169
+https://en.wikipedia.org/wiki/Bitwise_operation 169
+https://en.wikipedia.org/wiki/Unobtrusive_JavaScript 159
+https://en.wikipedia.org/wiki/Factory_method_pattern 158
+https://en.wikipedia.org/wiki/List_of_HTTP_status_codes 154
+https://en.wikipedia.org/wiki/Data_URI_scheme 154
+https://en.wikipedia.org/wiki/Short-circuit_evaluation 153
+https://en.wikipedia.org/wiki/Newline 153
+https://en.wikipedia.org/wiki/Don't_repeat_yourself 150
+https://en.wikipedia.org/wiki/Sorting_algorithm 144
+https://en.wikipedia.org/wiki/JSON 142
+https://en.wikipedia.org/wiki/Comma-separated_values 139
+https://en.wikipedia.org/wiki/C%2B%2B11 135
+https://en.wikipedia.org/wiki/Resource_Acquisition_Is_Initialization 133
+https://en.wikipedia.org/wiki/Modulo_operation 133
+https://en.wikipedia.org/wiki/Most_vexing_parse 131
+https://en.wikipedia.org/wiki/Dependency_injection 127
+https://en.wikipedia.org/wiki/ASCII 124
+https://en.wikipedia.org/wiki/First_normal_form 123
+https://en.wikipedia.org/wiki/Return_value_optimization 121
+https://en.wikipedia.org/wiki/Observer_pattern 121
+https://en.wikipedia.org/wiki/Unix_time 120
+
+Inspired by "MapReduce Design Patterns" by Donald Miner, Adam Shook http://shop.oreilly.com/product/0636920025122.do
+
+I'm using the MRDPutils module from the book, can be found here : https://github.com/adamjshook/mapreducepatterns/blob/master/MRDP/src/main/java/mrdp/utils/MRDPUtils.java
---
+++ WikipediaCounts.java
@@ -0,0 +1,236 @@
+package richard.nicolas.summarization;
+
+// Find most referenced Wikipedia page in Stackoverflow comments
+
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+
+import utils.MRDPUtils;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.filecache.DistributedCache;
+
+
+public class WikipediaCounts {
+
+    public static Boolean checkURLIsInCache(String text,
+                                            Set<String> set,
+                                            org.apache.hadoop.mapreduce.Mapper.Context context,
+                                            String counter_group) {
+        if (set.contains(text)) {
+            System.out.println("[+] Fnd in Cache :\t" + text);
+            context.getCounter(counter_group, "Cache_Hits").increment(1);
+            return true;
+        }
+        return false;
+    }
+
+    public static Boolean checkURLIsReachable(String result,
+                                              org.apache.hadoop.mapreduce.Mapper.Context context,
+                                              String counter_group) {
+        try {
+            URL url = new URL(result);
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            Integer statusCode = http.getResponseCode();
+            context.getCounter(counter_group, statusCode.toString()).increment(1);
+            // System.out.println(statusCode);
+            if (statusCode.equals(200)) {
+                System.out.println("[+] URL reached :\t" + result);
+                return true;
+            }
+        } catch (MalformedURLException e) {
+            // the URL is not in a valid form
+        } catch (IOException e) {
+            // the connection couldn't be established
+        }
+        return false;
+    }
+
+    public static String getWikipediaURL(String text,
+                                         org.apache.hadoop.mapreduce.Mapper.Context context,
+                                         String counter_group, Set<String> set) {
+
+        int idx = text.indexOf("http://en.wikipedia.org/wiki/");
+        if (idx == -1) {
+            return null;
+        } else {
+            // A wikipedia URL has been found. Increment the counter
+            context.getCounter(counter_group, "Wikipedia_URL_Found").increment(1);
+        }
+
+        String result = text.substring(idx, text.length());
+
+        // Remove anything after an HTML anchor
+        int idx_hash = text.indexOf('#', idx + 1);
+        if (idx_hash != -1) {
+            result = text.substring(idx, idx_hash);
+        }
+
+        // Remove anything after a whitespace
+        result = result.split("\\s+")[0];
+
+        // Lots of URLs in the data are 'http' but now Wikipedia always redirects to 'https'
+        result = result.replaceAll("^http", "https");
+
+        // Lots of URLs can be validated after removing a trailing character or 2
+        ArrayList<String> URLsToTest = new ArrayList<String>();
+        URLsToTest.add(result);
+        URLsToTest.add(result.substring(0, result.length() - 1));
+        URLsToTest.add(result.substring(0, result.length() - 2));
+
+        System.out.println("[-] Processing :\t" + result);
+
+        // Test if any version of this URL is in the cache
+        for (String URLToTest : URLsToTest) {
+            if (checkURLIsInCache(URLToTest, set, context, counter_group)) {
+                return URLToTest;
+            }
+        }
+
+        // Test if any version of this URL is reachable
+        for (String URLToTest : URLsToTest) {
+            if (checkURLIsReachable(URLToTest, context, counter_group)) {
+                return URLToTest;
+            }
+        }
+        System.out.println("[!] URL processing failed :\t" + result);
+        return null;
+    }
+
+
+    public static class SOWikipediaExtractor extends
+            Mapper<Object, Text, Text, IntWritable> {
+
+        public static final String COUNTER_GROUP_NAME = "Counters";
+        private final static IntWritable one = new IntWritable(1);
+        private Set knownValidURLs = new HashSet();
+        private Text link = new Text();
+
+        // setup method is called by every mapper before starting, load the known URLs into a set
+        @Override
+        protected void setup(Context context) throws IOException,
+                InterruptedException {
+            URI[] files = DistributedCache.getCacheFiles(context
+                    .getConfiguration());
+
+            // if the files in the distributed cache are set
+            if (files != null && files.length == 1) {
+                System.out.println("Reading cached URLs from: " + files[0].getPath());
+
+                try {
+                    File f = new File(files[0].getPath());
+                    BufferedReader b = new BufferedReader(new FileReader(f));
+                    String readLine = "";
+                    System.out.println("Reading cached URLs in Buffered Reader");
+                    while ((readLine = b.readLine()) != null) {
+                        //System.out.println(readLine);
+                        knownValidURLs.add(readLine);
+                    }
+
+                    System.out.println("Length of the set created of the Cached URLs: " + knownValidURLs.size());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                throw new IOException(
+                        "Cache file not set in the DistributedCache.");
+            }
+        }
+
+        public void map(Object key, Text value, Context context)
+                throws IOException, InterruptedException {
+
+            // Parse the input string into a nice map
+            Map<String, String> parsed = MRDPUtils.transformXmlToMap(value
+                    .toString());
+
+            context.getCounter(COUNTER_GROUP_NAME, "All").increment(1);
+
+            // Grab the necessary XML attributes
+            String txt = parsed.get("Text");
+            String strDate = parsed.get("CreationDate");
+
+            if (txt == null) {
+                return;
+            }
+
+            String url = getWikipediaURL(txt, context, COUNTER_GROUP_NAME, knownValidURLs);
+            if (url == null) {
+                return;
+            }
+
+            System.out.println("[+] Sent record to reducer :\t" + strDate);
+
+            link.set(url);
+            context.write(link, one);
+        }
+    }
+
+    public static class IntSumReducer extends
+            Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+
+            result.set(sum);
+            context.write(key, result);
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args)
+                .getRemainingArgs();
+        if (otherArgs.length != 3) {
+            System.err.println("Usage: WikipediaCounts <in> <cachefile> <out>");
+            System.exit(2);
+        }
+        Job job = new Job(conf, "StackOverflow Wikipedia URL Count");
+        job.setJarByClass(WikipediaIndex.class);
+        job.setMapperClass(SOWikipediaExtractor.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
+
+        DistributedCache.addCacheFile(
+                FileSystem.get(conf).makeQualified(new Path(otherArgs[1]))
+                        .toUri(), job.getConfiguration());
+
+        int code = job.waitForCompletion(true) ? 0 : 1;
+        if(code==0){
+        for (Counter counter : job.getCounters().getGroup(
+                SOWikipediaExtractor.COUNTER_GROUP_NAME)) {
+            System.out.println(counter.getDisplayName() + "\t"
+                    + counter.getValue());
+            }
+        }
+        System.exit(code);
+    }
+
+}
