19a6da97dd688ee4bc8a10227f70d2a94c6f200e
Nicolas Richard <nrichard@radiumone.com>
2017-07-27 16:13:56 -0700

    Refactor _ static methods (checkUrl*, getWikipediaURL) become class methods of class SOWikipediaExtractor _ replace use of deprecated classes

--- WikipediaCounts.java
+++ WikipediaCounts.java
@@ -11,7 +11,6 @@ import java.util.Map;
 
 import utils.MRDPUtils;
 
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -23,110 +22,23 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.mapreduce.Counter;
-import org.apache.hadoop.filecache.DistributedCache;
-
 
 public class WikipediaCounts {
-
-    public static Boolean checkURLIsInCache(String text,
-                                            Set<String> set,
-                                            org.apache.hadoop.mapreduce.Mapper.Context context,
-                                            String counter_group) {
-        if (set.contains(text)) {
-            System.out.println("[+] Fnd in Cache :\t" + text);
-            context.getCounter(counter_group, "Cache_Hits").increment(1);
-            return true;
-        }
-        return false;
-    }
-
-    public static Boolean checkURLIsReachable(String result,
-                                              org.apache.hadoop.mapreduce.Mapper.Context context,
-                                              String counter_group) {
-        try {
-            URL url = new URL(result);
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            Integer statusCode = http.getResponseCode();
-            context.getCounter(counter_group, statusCode.toString()).increment(1);
-            // System.out.println(statusCode);
-            if (statusCode.equals(200)) {
-                System.out.println("[+] URL reached :\t" + result);
-                return true;
-            }
-        } catch (MalformedURLException e) {
-            // the URL is not in a valid form
-        } catch (IOException e) {
-            // the connection couldn't be established
-        }
-        return false;
-    }
-
-    public static String getWikipediaURL(String text,
-                                         org.apache.hadoop.mapreduce.Mapper.Context context,
-                                         String counter_group, Set<String> set) {
-
-        int idx = text.indexOf("http://en.wikipedia.org/wiki/");
-        if (idx == -1) {
-            return null;
-        } else {
-            // A wikipedia URL has been found. Increment the counter
-            context.getCounter(counter_group, "Wikipedia_URL_Found").increment(1);
-        }
-
-        String result = text.substring(idx, text.length());
-
-        // Remove anything after an HTML anchor
-        int idx_hash = text.indexOf('#', idx + 1);
-        if (idx_hash != -1) {
-            result = text.substring(idx, idx_hash);
-        }
-
-        // Remove anything after a whitespace
-        result = result.split("\\s+")[0];
-
-        // Lots of URLs in the data are 'http' but now Wikipedia always redirects to 'https'
-        result = result.replaceAll("^http", "https");
-
-        // Lots of URLs can be validated after removing a trailing character or 2
-        ArrayList<String> URLsToTest = new ArrayList<String>();
-        URLsToTest.add(result);
-        URLsToTest.add(result.substring(0, result.length() - 1));
-        URLsToTest.add(result.substring(0, result.length() - 2));
-
-        System.out.println("[-] Processing :\t" + result);
-
-        // Test if any version of this URL is in the cache
-        for (String URLToTest : URLsToTest) {
-            if (checkURLIsInCache(URLToTest, set, context, counter_group)) {
-                return URLToTest;
-            }
-        }
-
-        // Test if any version of this URL is reachable
-        for (String URLToTest : URLsToTest) {
-            if (checkURLIsReachable(URLToTest, context, counter_group)) {
-                return URLToTest;
-            }
-        }
-        System.out.println("[!] URL processing failed :\t" + result);
-        return null;
-    }
-
 
     public static class SOWikipediaExtractor extends
             Mapper<Object, Text, Text, IntWritable> {
 
-        public static final String COUNTER_GROUP_NAME = "Counters";
+        private static Context context;
+        private static final String COUNTER_GROUP_NAME = "Counters";
+        private static Set<String> knownValidURLs = new HashSet<String>();
         private final static IntWritable one = new IntWritable(1);
-        private Set knownValidURLs = new HashSet();
         private Text link = new Text();
 
         // setup method is called by every mapper before starting, load the known URLs into a set
         @Override
-        protected void setup(Context context) throws IOException,
-                InterruptedException {
-            URI[] files = DistributedCache.getCacheFiles(context
-                    .getConfiguration());
+        protected void setup(Context context) throws IOException, InterruptedException {
+            SOWikipediaExtractor.context = context;
+            URI[] files = context.getCacheFiles();
 
             // if the files in the distributed cache are set
             if (files != null && files.length == 1) {
@@ -135,7 +47,7 @@ public class WikipediaCounts {
                 try {
                     File f = new File(files[0].getPath());
                     BufferedReader b = new BufferedReader(new FileReader(f));
-                    String readLine = "";
+                    String readLine;
                     System.out.println("Reading cached URLs in Buffered Reader");
                     while ((readLine = b.readLine()) != null) {
                         //System.out.println(readLine);
@@ -171,7 +83,7 @@ public class WikipediaCounts {
                 return;
             }
 
-            String url = getWikipediaURL(txt, context, COUNTER_GROUP_NAME, knownValidURLs);
+            String url = getWikipediaURL(txt);
             if (url == null) {
                 return;
             }
@@ -181,6 +93,85 @@ public class WikipediaCounts {
             link.set(url);
             context.write(link, one);
         }
+
+        public static String getWikipediaURL(String text) {
+
+            int idx = text.indexOf("http://en.wikipedia.org/wiki/");
+            if (idx == -1) {
+                return null;
+            } else {
+                // A wikipedia URL has been found. Increment the counter
+                context.getCounter(COUNTER_GROUP_NAME, "Wikipedia_URL_Found").increment(1);
+            }
+
+            String result = text.substring(idx, text.length());
+
+            // Remove anything after an HTML anchor
+            int idx_hash = text.indexOf('#', idx + 1);
+            if (idx_hash != -1) {
+                result = text.substring(idx, idx_hash);
+            }
+
+            // Remove anything after a whitespace
+            result = result.split("\\s+")[0];
+
+            // Lots of URLs in the data are 'http' but now Wikipedia always redirects to 'https'
+            result = result.replaceAll("^http", "https");
+
+            // Lots of URLs can be validated after removing a trailing character or 2
+            ArrayList<String> URLsToTest = new ArrayList<String>();
+            URLsToTest.add(result);
+            URLsToTest.add(result.substring(0, result.length() - 1));
+            URLsToTest.add(result.substring(0, result.length() - 2));
+
+            System.out.println("[-] Processing :\t" + result);
+
+            // Test if any version of this URL is in the cache
+            for (String URLToTest : URLsToTest) {
+                if (checkURLIsInCache(URLToTest)) {
+                    return URLToTest;
+                }
+            }
+
+            // Test if any version of this URL is reachable
+            for (String URLToTest : URLsToTest) {
+                if (checkURLIsReachable(URLToTest)) {
+                    return URLToTest;
+                }
+            }
+            System.out.println("[x] URL processing failed :\t" + result);
+            return null;
+        }
+
+        public static Boolean checkURLIsInCache(String text) {
+            if (knownValidURLs.contains(text)) {
+                System.out.println("[+] Fnd in Cache :\t" + text);
+                context.getCounter(COUNTER_GROUP_NAME, "Cache_Hits").increment(1);
+                return true;
+            }
+            return false;
+        }
+
+        public static Boolean checkURLIsReachable(String result) {
+            try {
+                URL url = new URL(result);
+                HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                Integer statusCode = http.getResponseCode();
+                context.getCounter(COUNTER_GROUP_NAME, statusCode.toString()).increment(1);
+                // System.out.println(statusCode);
+                if (statusCode.equals(200)) {
+                    System.out.println("[+] URL reached :\t" + result);
+                    return true;
+                }
+            } catch (MalformedURLException e) {
+                // the URL is not in a valid form
+            } catch (IOException e) {
+                // the connection couldn't be established
+            }
+            return false;
+        }
+
+
     }
 
     public static class IntSumReducer extends
@@ -208,7 +199,8 @@ public class WikipediaCounts {
             System.err.println("Usage: WikipediaCounts <in> <cachefile> <out>");
             System.exit(2);
         }
-        Job job = new Job(conf, "StackOverflow Wikipedia URL Count");
+        Job job = Job.getInstance();
+        job.setJobName("StackOverflow Wikipedia URL Count");
         job.setJarByClass(WikipediaIndex.class);
         job.setMapperClass(SOWikipediaExtractor.class);
         job.setCombinerClass(IntSumReducer.class);
@@ -218,16 +210,14 @@ public class WikipediaCounts {
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
         FileOutputFormat.setOutputPath(job, new Path(otherArgs[2]));
 
-        DistributedCache.addCacheFile(
-                FileSystem.get(conf).makeQualified(new Path(otherArgs[1]))
-                        .toUri(), job.getConfiguration());
+        job.addCacheFile(new Path(otherArgs[1]).toUri());
 
         int code = job.waitForCompletion(true) ? 0 : 1;
         if(code==0){
-        for (Counter counter : job.getCounters().getGroup(
-                SOWikipediaExtractor.COUNTER_GROUP_NAME)) {
-            System.out.println(counter.getDisplayName() + "\t"
-                    + counter.getValue());
+            for (Counter counter : job.getCounters().getGroup(
+                    SOWikipediaExtractor.COUNTER_GROUP_NAME)) {
+                System.out.println(counter.getDisplayName() + "\t"
+                        + counter.getValue());
             }
         }
         System.exit(code);
